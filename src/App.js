5b18a54e7b156111f34a9e0ad2f763d3d0fa5347
Rony Derra <derrarony@gmail.com>
2022-03-26 02:39:14 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import tableDragger from "table-dragger";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSelector } from 'react-redux'
 
-function App() {
+export default function App() {
+  const employeeTableData = useSelector((state) => state.employeeTable.data)
+  const [employees, setEmployees] = useState(employeeTableData);
+
+
+  useEffect(() => {
+    var el = document.getElementById("table");
+    var dragger = tableDragger(el, {
+      mode: "free",
+      onlyBody: false,
+      dragHandler: ".handle",
+      animation: 300
+    });
+    dragger.on("drop", function (from, to) {
+      console.log(from);
+    });
+  }, []);
+
+
   return (
-    <div className="App">
-
+    <div className="container">
+      <h1>The Big Bang Task</h1>
+      <table className="table" id="table">
+        <thead>
+          <tr>
+            <th className="handle" ></th>
+            <th className="handle" >אפשרויות</th>
+            <th className="handle">סך הכל שעות</th>
+            <th className="handle">שעות</th>
+            <th className="handle">שעות חריגות</th>
+            <th className="handle">שם עובד</th>
+            <th className="handle">מספר תז</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees?.map((employe, index) => (
+            <tr key={index}>
+             <th className="handle" scope="row">{index}</th>
+              <td ></td>
+              <td>{employe.sumHours}</td>
+              <td>{employe.hours}</td>
+              <td>{employe.overLimitHours}</td>
+              <td>{employe.name}</td>
+              <td>{employe.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
