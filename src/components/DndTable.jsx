7cd411b5d699a4f6fce0946e0a7d40b0f00844eb
Rony Derra <derrarony@gmail.com>
2022-03-26 21:07:44 +0300

    changes

--- src/components/DndTable.jsx
+++ src/components/DndTable.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import Ellipse from '../assets/Ellipse.png'
 import ThreeDots from '../assets/ThreeDots.png'
 import BarChart from '../assets/BarChart.png'
-import "./DndTable.css";
 import tableDragger from "table-dragger";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useSelector, useDispatch } from 'react-redux'
 import { Container, Table, Button } from "react-bootstrap";
 import { addRow } from "../redux/employeeSlice";
+import "./DndTable.css";
 
 const DndTable = () => {
   const employeeTableData = useSelector((state) => state.employeeTable)
@@ -26,7 +26,7 @@ const DndTable = () => {
   return (
     <Container>
       <h1 className="title">Drag and Drop React-Table</h1>
-      <Table responsive id="table" size="sm">
+      <Table responsive id="table" size="sm" >
         <thead>
           <tr>
             <th className="handle"></th>
@@ -36,31 +36,31 @@ const DndTable = () => {
             <th className="handle">שעות ידניות</th>
             <th className="handle">שעות חריגות</th>
             <th className="handle">שם עובד</th>
-            <th className="handle">מספר תז</th>
+            <th className="handle sagiv">מספר תז</th>
           </tr>
         </thead>
         <tbody>
           {employeeTableData?.map((employee, index) => (
             <tr key={index} >
-              <th className="handle" scope="row"><img src={ThreeDots} /></th>
+              <td className="handle" scope="row"><img src={ThreeDots} /></td>
               <td className="noWidth"><img src={BarChart} /></td>
               <td>{employee.sumHours}</td>
               <td>{employee.hours}</td>
               <td>{employee.overLimitHours}</td>
               <td>{employee.manualHours}</td>
-              <td>{employee.name} <span><img src={Ellipse} /></span></td>
+              <td>{employee.name} <span><img src= {bool ? Ellipse : 6767}  /></span></td>
               <td>{employee.id}</td>
             </tr>
           ))}
         </tbody>
       </Table>
       <Button onClick={() => dispatch(addRow({
-        "id": 316287341,
-        "name": "שלמה",
-        "overLimitHours": '4:00',
-        "manualHours": '4:00',
-        "hours": '155:00',
-        "sumHours": '159:00'
+        id: 316287341,
+        name: "שלמה",
+        overLimitHours: '4:00',
+        manualHours: '4:00',
+        hours: '155:00',
+        sumHours: '159:00'
       }))}>Add Row</Button>
     </Container>
   );
